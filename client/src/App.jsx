c82727089f033171043fbdcb74336e5ed0a2bf85
Shailly-Sahay <shaillysahayy@gmail.com>
2025-01-27 12:32:30 +0530

    feat: implement the custom button on the home page to moce to customizer page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import { Home, Customizer } from "./pages";
 function App() {
   return (
     <main className="app-transition-all ease-in">
-      <h1 className="head-text">ThreeJs</h1>
       <Home />
       <Canvas />
       <Customizer />

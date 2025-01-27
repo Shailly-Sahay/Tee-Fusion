f8246c69491c65fa5e2f3a5afeab4d6a3f9b9ba2
Shailly-Sahay <shaillysahayy@gmail.com>
2025-01-27 12:50:53 +0530

    feat: implement tab section in the customizer page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,5 @@
 export { default as CustomButton } from "./CustomButton.jsx";
+export { default as AIPicker } from "./AIPicker.jsx";
+export { default as ColorPicker } from "./ColorPicker.jsx";
+export { default as FilePicker } from "./FilePicker.jsx";
+export { default as Tab } from "./Tab.jsx";
